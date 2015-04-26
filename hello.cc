955d781ea97cd8563515d57cda8b7d2bb246a9ca
Moshe Kolodny <kolodny@github.com>
2015-04-25 23:32:31 -0400

    tried from iojs docs, same error

--- hello.cc
+++ hello.cc
@@ -1,16 +1,25 @@
 // hello.cc
 #include <node.h>
 
-using namespace v8;
+namespace demo {
+
+using v8::FunctionCallbackInfo;
+using v8::HandleScope;
+using v8::Isolate;
+using v8::Local;
+using v8::Object;
+using v8::String;
+using v8::Value;
 
 void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = Isolate::GetCurrent();
-  HandleScope scope(isolate);
+  Isolate* isolate = args.GetIsolate();
   args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
 }
 
-void init(Handle<Object> exports) {
+void init(Local<Object> exports) {
   NODE_SET_METHOD(exports, "hello", Method);
 }
 
 NODE_MODULE(addon, init)
+
+}  // namespace demo
